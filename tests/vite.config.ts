d355a023298b6bc347a7bc84015d5d0aaae706c0
ZtaDev <85247633+ZtaMDev@users.noreply.github.com>
2026-01-13 18:08:06 -0500

    fix vite compat(again)

--- tests/vite.config.ts
+++ tests/vite.config.ts
@@ -1,19 +1,18 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@pulse-js/core': resolve(__dirname, '../src/index.ts'),
-      '@pulse-js/react': resolve(__dirname, '../packages/react/src/index.ts'),
-      "@pulse-js/tools": resolve(__dirname, "../packages/tools/src/index.tsx")
-    }
+      "@pulse-js/core": resolve(__dirname, "../src/index.ts"),
+      "@pulse-js/react": resolve(__dirname, "../packages/react/src/index.ts"),
+      "@pulse-js/tools": resolve(__dirname, "../packages/tools/src/index.tsx"),
+    },
   },
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
