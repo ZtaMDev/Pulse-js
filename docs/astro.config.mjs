6b9926955d486333c23d9e7a49bb6c6a43005278
ZtaDev <85247633+ZtaMDev@users.noreply.github.com>
2026-01-17 12:17:34 -0500

    docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ztamdev.github.io/pulse-js',
+	base: '/pulse-js',
 	integrations: [
 		starlight({
 			title: 'Pulse-JS',
